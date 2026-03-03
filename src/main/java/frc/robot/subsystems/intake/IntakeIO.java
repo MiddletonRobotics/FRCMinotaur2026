8f4a3aa27b371235fcf24693e2098f8122f425c9
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-03-02 23:18:01 -0500

    added intake io

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -0,0 +1,48 @@
+package frc.robot.subsystems.intake;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface IntakeIO {
+    @AutoLog
+    public class IntakeIOInputs {
+        public boolean rollerMotorConnected = true;
+        public double rollerPosition = 0.0;
+        public double rollerVelocity = 0.0;
+        public double rollerAcceleration = 0.0;
+        public double rollerAppliedVoltage = 0.0;
+        public double rollerSupplyCurrentAmperes = 0.0;
+        public double rollerTorqueCurrentAmperes = 0.0;
+        public double rollerTemperatureCelsius = 0.0;
+
+        public boolean pivotMotorConnected = true;
+        public double pivotPosition = 0.0;
+        public double pivotVelocity = 0.0;
+        public double pivotAcceleration = 0.0;
+        public double pivotAppliedVoltage = 0.0;
+        public double pivotSupplyCurrentAmperes = 0.0;
+        public double pivotMotorTempuratureCelcius = 0.0;
+    }
+
+    public enum IntakeControlMode {
+        BRAKE,
+        COAST,
+        VOLTAGE_CONTROL,
+        VELOCITY_CONTROLLED
+    }
+
+    public default void updateInputs(IntakeIOInputs inputs) {}
+
+    public default void setPivotVoltage(double voltage) {}
+
+    public default void setRollerVoltage(double voltage) {}
+
+    public default void setPivotPosition(double position) {}
+
+    public default void setRollerTorqueCurrent(double amperes) {}
+
+    public default void setRollerVelocity(double velocity) {}
+
+    public default void stopRollers() {}
+
+    public default void setBrakeMode(boolean enabled) {}
+}
