5792ca8455a028c3c70592c84077b208078cbfbe
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-03-14 23:57:46 -0400

    reordered some commands (dont use these anymore)

--- src/main/java/frc/robot/command_factories/IntakeFactory.java
+++ src/main/java/frc/robot/command_factories/IntakeFactory.java
@@ -2,7 +2,7 @@ package frc.robot.command_factories;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-
+import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.intake.Intake.PivotGoal;
 import frc.robot.subsystems.intake.Intake.RollerGoal;
@@ -10,22 +10,18 @@ import frc.robot.subsystems.intake.Intake.RollerGoal;
 public class IntakeFactory {
     public static Command prepareIntakeBlocking(RobotContainer robotContainer) {
         return Commands.parallel(
-            Commands.runOnce(() -> robotContainer.getIntake().setPivotGoal(PivotGoal.DEPLOY)),
-            Commands.waitUntil(robotContainer.getIntake()::pivotAtGoal)
-                .andThen(Commands.startEnd(
-                    () -> robotContainer.getIntake().setRollerGoal(RollerGoal.INTAKE),
-                    () -> robotContainer.getIntake().setRollerGoal(RollerGoal.STOP)
-                ))
+            Commands.runOnce(() -> robotContainer.getIntake().setPivotGoal(PivotGoal.DEPLOY)).until(robotContainer.getIntake()::pivotAtGoal),
+            Commands.startEnd(
+                () -> robotContainer.getIntake().setRollerGoal(RollerGoal.INTAKE),
+                () -> robotContainer.getIntake().setRollerGoal(RollerGoal.STOP)
+            ).withInterruptBehavior(InterruptionBehavior.kCancelIncoming)
         );
     }
 
     public static Command parkIntakeBlocking(RobotContainer robotContainer) {
         return Commands.parallel(
-            Commands.runOnce(() -> robotContainer.getIntake().setPivotGoal(PivotGoal.PARKED))
-                .alongWith(Commands.runOnce(
-                    () -> robotContainer.getIntake().setRollerGoal(RollerGoal.IDLE)
-                )),
-            Commands.waitUntil(robotContainer.getIntake()::pivotAtGoal)
-        );
+            Commands.runOnce(() -> robotContainer.getIntake().setPivotGoal(PivotGoal.PARKED)),
+            Commands.runOnce(() -> robotContainer.getIntake().setRollerGoal(RollerGoal.STOP))
+        ).until(robotContainer.getIntake()::pivotAtGoal).withName("Retract Intake");
     }
 }
